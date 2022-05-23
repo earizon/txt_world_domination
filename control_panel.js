3ef836dbd6289a6ed1162a55c9db5431eebd874b
Enrique Arizón Benito <enrique.arizonbenito@gmail.com>
2022-05-23 22:50:55 +0200

    WiP: Add grep engine

--- control_panel.js
+++ control_panel.js
@@ -1,33 +1,41 @@
 // import { html, Component, render } from 'https://unpkg.com/htm/preact/standalone.module.js';
 import { html, Component, render } from './preact.standalone.module.js';
 
-
-function doSearch(grepInput) {
-    alert(grepInput)
-}
+import { TXTDBEngine } from './txt_ddbb_engine.js';
 
 class ControlPanel extends Component {
 
     state = {
-      grepInput   : "",
-      timerDoFind : 0
+      timerDoFind : 0,
+      dbEngineOutput : ""
     }
 
-    constructor({ grepInput }) {
-      super({ grepInput });
-      this.state.grepInput = grepInput
+    constructor({ payload }) {
+      super({ payload });
+      this.state.dbEngineOutput = payload
+      this.txtDBEngine = new TXTDBEngine(payload)
+      new TXTDBEngine(payload)
     }
 
     onGrepInputChanged = (inputEvent) => {
       if (this.state.timerDoFind !== null) {
         clearTimeout(this.state.timerDoFind)
       }
-      this.state.timerDoFind = setTimeout(() => doSearch(inputEvent.target.value), 1000)
+      this.state.timerDoFind = setTimeout(
+        () => {
+console.log("1 "+inputEvent.target.value)
+          const dbEngineOutput = this.txtDBEngine.grep(inputEvent.target.value)
+console.log("1 "+dbEngineOutput)
+          this.setState ( { dbEngineOutput :  dbEngineOutput } )
+        }, 1000)
     }
 
     render( props ) {
       return html`
-        grep: <input value='${this.state.grepInput}' onInput=${ (e) => this.onGrepInputChanged(e) } ></input>
+        grep:<input value='${this.state.grepInput}'
+              onInput=${ (e) => this.onGrepInputChanged(e) } ></input>
+        <hr/>
+        ${this.state.dbEngineOutput}
       `;
     }
 }
---
+++ txt_ddbb_engine.js
@@ -0,0 +1,14 @@
+class TXTDBEngine  {
+
+    constructor( payload ) {
+        this.payload = payload
+    }
+
+    grep( grepInput ) {
+        return this.payload // TODO:(0)
+    }
+}
+
+export{
+  TXTDBEngine
+}
