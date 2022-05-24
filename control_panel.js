465203ee5df67c136d10249cbd4505466b4fb4c6
Enrique Arizón Benito <enrique.arizonbenito@gmail.com>
2022-05-24 21:49:21 +0200

    WiP: Grep support

--- control_panel.js
+++ control_panel.js
@@ -18,24 +18,64 @@ class ControlPanel extends Component {
       new TXTDBEngine(payload)
     }
 
-    onGrepInputChanged = (inputEvent) => {
+    onGrepInputChanged = () => {
       if (this.state.timerDoFind !== null) {
         clearTimeout(this.state.timerDoFind)
       }
       this.state.timerDoFind = setTimeout(
         () => {
-          this.state.grep[0].input = inputEvent.target.value
           const dbEngineOutput = this.txtDBEngine.grep(this.state.grep[0])
-          this.setState ( { dbEngineOutput :  dbEngineOutput, grep : this.state.grep } )
+          this.setState ( { dbEngineOutput : dbEngineOutput } )
         }, 200)
     }
 
+    onGrepRegexChanged = (inputEvent) => {
+      this.state.grep[0].input = inputEvent.target.value
+      this.onGrepInputChanged()
+    }
+
+    doTxtPreProcessing = (input) => {
+       let H = input
+       // NEXT) replace html scape chars 
+    // H = H.replaceAll('<','&lt;') 
+    //      .replaceAll('>','&gt;')
+       // NEXT) replace External links
+       H = H.replace(
+           /@\[((http|[.][/]).?[^\]]*)\]/g,
+           " ▶<a target='_blank' href='$1'>$1</a>◀")   
+    
+       // NEXT) replace relative (to page) link
+       H = H.replace(
+           /@\[([^\]]*)\]/g,
+           " ▷<a href='$1'>$1</a>◁")   
+       return H
+    }
+
+    componentDidMount() { this.componentDidUpdate() }
+
+    componentDidUpdate = () => {
+      document.getElementById("dbEngineOutput").innerHTML =
+           this.doTxtPreProcessing(this.state.dbEngineOutput)
+    }
+
+    setGrepBound = (flag, delta) => {
+      if ( flag == 'b'/*before*/ ) this.state.grep[0].before += delta
+      if ( flag == 'a'/*after */ ) this.state.grep[0].after  += delta
+      this.setState ( { grep : this.state.grep } )
+      this.onGrepInputChanged()
+    }
     render( props ) {
       return html`
-        grep:<input value='${this.state.grep[0].input}'
-              onInput=${ (e) => this.onGrepInputChanged(e) } ></input>
-        <hr/>
-        ${this.state.dbEngineOutput}
+        grep:
+          <span onClick=${ (e) => this.setGrepBound('b',-1)}>-</span>
+          ${this.state.grep[0].before}
+          <span onClick=${ (e) => this.setGrepBound('b',+1)}>+</span>
+            <input value='${this.state.grep[0].input}'
+              onInput=${ (e) => this.onGrepRegexChanged(e) } >
+             </input>${this.state.after}
+          <span onClick=${ (e) => this.setGrepBound('a',-1)}>-</span>
+          ${this.state.grep[0].after }
+          <span onClick=${ (e) => this.setGrepBound('a',+1)}>+</span>
       `;
     }
 }
--- txt_ddbb_engine.js
+++ txt_ddbb_engine.js
@@ -8,7 +8,7 @@ class RingBuffer {
 
     // https://stackoverflow.com/questions/1583123/circular-buffer-in-javascript
     
-    get   = function(){return this.buffer.slice(0,this.pointer)};
+    get   = function(){return this.buffer.slice(0,this.pointer+1)};
     push  = function(item){
        this.buffer[this.pointer] = item;
        this.pointer = (this.length + this.pointer +1) % this.length;
@@ -26,15 +26,25 @@ class TXTDBEngine  {
     grep( grep0 ) {
       const grepInput = grep0.input
       if (!!! grepInput ) return this.payload
-      const beforeRB  = new RingBuffer(grep0.before)
+      const beforeRB        = new RingBuffer(grep0.before)
       const result = [];
+      let afterPending = 0
       this.payload.split("\n").forEach( lineN => {
-        let isMatch = lineN.indexOf(grepInput) > 0
-// console.log(isMatch +","+ lineN)
+        let isMatch = (lineN.indexOf(grepInput) > 0) 
+
+        if ( !isMatch && afterPending > 0 ) {
+            afterPending=afterPending-1
+            result.push(lineN)
+            if (afterPending==0) {
+              result.push("<<<")
+            }
+        }
         if (isMatch) {
-           result.push(beforeRB.get())
+           result.push(">>>")
+           beforeRB.get().forEach(bLine => result.push(bLine))
            beforeRB.reset()
            result.push(lineN)
+           afterPending = grep0.after
         } else {
            beforeRB.push(lineN)
         }
