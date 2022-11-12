fa937e532e911ad54bea37810b547fd82bd00cf6
Enrique Arizón Benito <enrique.arizonbenito@gmail.com>
2022-11-12 19:46:59 +0100

    Improve script

--- tags.sh
+++ tags.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-cat $* | \
+INPUT_L=""
+if [[ $# == 0 ]]; then
+   INPUT_L=$(find ./ -maxdepth 2 -type f -iname "*txt") # def. to all files in dir and subdirs.
+else
+   INPUT_L="$*"
+fi
+cat ${INPUT_L} | \
 grep -E "\[\[[^]]*\]\]" | \
  sed -E "s/^.*\[\[[{]?//" | \
  sed -E "s/[}]?\]\].*//" | \
  tr "," "\n" | \
  grep -v "^$" | \
  grep -iv "binary.file" | \
+ sed "s/ *//" | \
  tr [:lower:] [:upper:]  | \
  sort | uniq | \
 cat -
