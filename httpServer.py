a2a132105f8f5aa2dc2771f8085b6d407812f85a
Enrique Arizón Benito <enrique.arizon@accenture.com>
2024-12-27 12:55:06 +0100

    Fork in background. Works on Linux/Mac, fails on Windows

--- httpServer.py
+++ httpServer.py
@@ -16,33 +16,59 @@
 import os, http.server
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 import socketserver
+import os
+import sys
+from pprint import pprint # debugging 101
 
 PORT = 9000
-os.chdir("..")
 
-Handler = http.server.SimpleHTTPRequestHandler
-Handler.protocol_version = "HTTP/1.1"
-# Handler.close_connection = True
+server=None
 
-Handler.extensions_map={
-        '.wasm': 'application/wasm',
-    '.manifest': 'text/cache-manifest',
-     '.payload': 'text/plain',
-	    '.html': 'text/html', '.png': 'image/png',
-	     '.jpg': 'image/jpg',
-	     '.svg': 'image/svg+xml',
-	     '.css': 'text/css',
-	      '.js': 'application/x-javascript',
-	      '.md': 'text/markdown',
-	      ''   : 'application/octet-stream', # Default
-}
+def initialization_function():
+  os.chdir("..")
+  
+  Handler = http.server.SimpleHTTPRequestHandler
+  Handler.protocol_version = "HTTP/1.1"
+  # Handler.close_connection = True
+  
+  Handler.extensions_map={
+          '.wasm': 'application/wasm',
+      '.manifest': 'text/cache-manifest',
+       '.payload': 'text/plain',
+  	    '.html': 'text/html', '.png': 'image/png',
+  	     '.jpg': 'image/jpg',
+  	     '.svg': 'image/svg+xml',
+  	     '.css': 'text/css',
+  	      '.js': 'application/x-javascript',
+  	      '.md': 'text/markdown',
+  	      ''   : 'application/octet-stream', # Default
+  }
+  
+  print(f"""
+  - Visit http://localhost:{PORT}/wallet_services "
+  """)
+  global server
+  server = ThreadingHTTPServer(("127.0.0.1", PORT), Handler)
 
+if __name__ != "__main__":
+   print("debug: executing as __main__" )
 
-print(f"""
-- Visit http://localhost:{PORT}/wallet_services "
-""")
+initialization_function()
 
-server = ThreadingHTTPServer(("127.0.0.1", PORT), Handler)
-server.serve_forever()
-
-os.system(f"xdg-open  http://127.0.0.1:{PORT}/txt_world_domination/index.html 1>/dev/null 2>&1 &")
+# Daemonize the process
+if os.fork() > 0:
+  URL_BASE="http://127.0.0.1:{PORT}"
+  INDEX_PATH="/txt_world_domination/index.html"
+  os.system(f"xdg-open {URL_BASE}/{INDEX_PATH} 1>/dev/null 2>&1 &")
+  sys.exit()
+else:
+  os.setsid() # TODO:(0)
+  # sys.stdout.flush()
+  # sys.stderr.flush()
+  # with open('/dev/null', 'rb', 0) as read_null:
+  #   with open('/dev/null', 'wb', 0) as write_null:
+  #     with open('/dev/null', 'wb', 0) as error_null:
+  #       os.dup2(read_null.fileno(), sys.stdin.fileno())
+  #       os.dup2(write_null.fileno(), sys.stdout.fileno())
+  #       os.dup2(error_null.fileno(), sys.stderr.fileno())
+  server.serve_forever()
